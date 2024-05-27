acace4f8dba3cdc3940667d3e76e9016d2b00723
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2024-05-27 06:49:01 -0500

    Update 347-Top-K-Frequent-Elements.py

--- medium/347-Top-K-Frequent-Elements.py
+++ medium/347-Top-K-Frequent-Elements.py
@@ -5,4 +5,21 @@ Given an integer array nums and an integer k, return the k most frequent element
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return sorted(set(nums), key=nums.count)[::-1][:k]
+        d, count, out = {}, {}, []
+
+        for i in nums:
+            count[i] = count.get(i, 0) + 1
+
+        for n, c in count.items():
+            if not d.get(c):
+                d[c] = [n]
+            elif n not in d[c]:
+                d[c].append(n)
+        
+        c = 0
+        for i in range(len(nums), -1, -1):
+            for j in d.get(i, []):
+                out.append(j)
+                c += 1
+                if c == k:
+                    return out
