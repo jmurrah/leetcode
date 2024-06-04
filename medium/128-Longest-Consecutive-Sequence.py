0df1dd96a9182e0c74e9795bb782b71147a586d9
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2024-06-04 13:13:50 -0500

    Create 128-Longest-Consecutive-Sequence.py

---
+++ medium/128-Longest-Consecutive-Sequence.py
@@ -0,0 +1,21 @@
+"""
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+"""
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        s, m = set(nums), 0
+
+        for i in s:
+            count = 1
+            if i-1 not in s:
+                current = i + 1
+                while current in s:
+                    count += 1
+                    current += 1
+                m = max(count, m)
+        
+        return m
