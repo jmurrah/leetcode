77f0091351e683616a78729f1732daca43a2605b
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2024-07-10 06:48:08 -0500

    Create 1598-Crawler-Log-Folder.py

---
+++ easy/1598-Crawler-Log-Folder.py
@@ -0,0 +1,28 @@
+"""
+The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+The operations are described below:
+
+    "../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+    "./" : Remain in the same folder.
+    "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+
+You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+The file system starts in the main folder, then the operations in logs are performed.
+
+Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+"""
+
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        output = 0
+
+        for log in logs:
+            if log.startswith("../"):
+                output -= 1 if output > 0 else 0
+            elif not log.startswith("./"):
+                output += 1
+            
+        return output
