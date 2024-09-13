6919ce9b04858275c65113e416fe338cf8f78ca3
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2024-09-12 20:47:39 -0500

    Create 371-Sum-of-Two-Integers.py

---
+++ medium/371-Sum-of-Two-Integers.py
@@ -0,0 +1,10 @@
+"""
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+"""
+
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # This is a stupid question
+        return sum([a, b])
+        
