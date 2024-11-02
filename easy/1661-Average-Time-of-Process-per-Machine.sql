55452e8da569041fc6908e5fe916d64c5e1e6aa4
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2024-11-01 20:30:08 -0500

    Update 1661-Average-Time-of-Process-per-Machine.sql

--- easy/1661-Average-Time-of-Process-per-Machine.sql
+++ easy/1661-Average-Time-of-Process-per-Machine.sql
@@ -11,7 +11,7 @@
 
 
 -- Write your MySQL query statement below
-select a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) as processing_time
-from activity a1, activity a2
-where a1.machine_id = a2.machine_id and a1.process_id = a2.process_id and a2.activity_type = 'end' and a1.activity_type = 'start'
-group by a1.machine_id
+SELECT a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+FROM activity a1, activity a2
+WHERE a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id AND a2.activity_type = 'end' AND a1.activity_type = 'start'
+GROUP BY a1.machine_id
