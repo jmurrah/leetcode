d9de56751e0aa379b23eedd49c8d95cd8b031ca4
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2025-01-07 20:46:29 -0600

    Update 189-Rotate-Array.py

--- medium/189-Rotate-Array.py
+++ medium/189-Rotate-Array.py
@@ -4,9 +4,19 @@ Given an integer array nums, rotate the array to the right by k steps, where k i
 
 
 class Solution:
+    def reverse(self, l, r, nums):
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        nums[k:], nums[:k] = nums[:-k], nums[-k:]
+        len_nums = len(nums)
+        k %= len_nums
+
+        self.reverse(0, len_nums-1, nums)
+        self.reverse(0, k-1, nums)
+        self.reverse(k, len_nums-1, nums)
