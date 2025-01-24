d4fee44e968d3887b6409a5ab1a64cd18061e4f0
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2025-01-23 19:52:37 -0600

    Create 802-Find-Eventual-Safe-States.py

---
+++ medium/802-Find-Eventual-Safe-States.py
@@ -0,0 +1,40 @@
+"""
+There is a directed graph of n nodes with each node labeled from 0 to n - 1. The graph is represented by a 0-indexed 
+2D integer array graph where graph[i] is an integer array of nodes adjacent to node i, meaning there is an edge from 
+node i to each node in graph[i].
+
+A node is a terminal node if there are no outgoing edges. A node is a safe node if every possible path starting from 
+that node leads to a terminal node (or another safe node).
+
+Return an array containing all the safe nodes of the graph. The answer should be sorted in ascending order.
+"""
+
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        # 1 = safe, 0 = visited, -1 = not safe
+        safe = [-2] * len(graph)
+
+        def find_safe(index):
+            if safe[index] == 1:
+                return True
+            if safe[index] == -1:
+                return False
+            if safe[index] == 0:
+                safe[index] = -1
+                return False
+
+            safe[index] = 0
+            for node in graph[index]:
+                if not find_safe(node):
+                    safe[index] = -1
+                    return False
+            
+            safe[index] = 1
+            return True
+        
+        for i in range(len(graph)):
+            find_safe(i)
+
+        return [i for i, flag in enumerate(safe) if flag == 1]
+        
