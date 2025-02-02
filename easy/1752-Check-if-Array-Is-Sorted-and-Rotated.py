becd71d60995b519067327e4e9b53ad720a68529
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2025-02-02 10:35:21 -0600

    Create 1752-Check-if-Array-Is-Sorted-and-Rotated.py

---
+++ easy/1752-Check-if-Array-Is-Sorted-and-Rotated.py
@@ -0,0 +1,20 @@
+"""
+Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+"""
+
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        is_rotated = False
+
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i-1]:
+                if is_rotated or nums[i] > nums[i-1]:
+                    return False
+                is_rotated = True
+        
+        return nums[0] >= nums[-1] if is_rotated else True
