1bfba45135a7548ccb5fe1b4b570c74243a82629
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2025-03-10 14:14:09 -0500

    Create 525-Contiguous-Array.py

---
+++ medium/525-Contiguous-Array.py
@@ -0,0 +1,19 @@
+"""
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+"""
+
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        d = {0: -1}
+        pref_sum = output = 0
+
+        for i, num in enumerate(nums):
+            pref_sum += 1 if num else -1
+
+            if pref_sum in d:
+                output = max(output, i - d[pref_sum])
+            else:
+                d[pref_sum] = i
+
+        return output
