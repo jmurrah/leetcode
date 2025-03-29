1a80dd00b4f6f9ade74eae91684a375c6a09927d
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2025-03-29 11:20:27 -0500

    Update 3499-Maximize-Active-Section-with-Trade-I.py

--- medium/3499-Maximize-Active-Section-with-Trade-I.py
+++ medium/3499-Maximize-Active-Section-with-Trade-I.py
@@ -17,36 +17,18 @@ Note: Treat s as if it is augmented with a '1' at both ends, forming t = '1' + s
 
 class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
-        s = "1" + s + "1"
-        len_s = len(s)
-        i = one = zero = 0
+        count = s.count("1")
+        best = prev = curr = 0
 
-        # skip initial 1s
-        while i < len_s and s[i] == '1':
-            one += 1
-            i += 1
-        
-        # count first block of 0s
-        prev = 0
-        while i < len_s and s[i] == '0':
-            prev += 1
-            i += 1
-        
-        # process the rest of the string
-        while i < len_s:
-            while i < len_s and s[i] == '1':
-                one += 1
-                i += 1
-
-            if i == len_s:
-                break
-
-            curr = 0
-            while i < len_s and s[i] == '0':
+        s += "1"
+        for i in range(len(s)):
+            if s[i] == "0":
                 curr += 1
-                i += 1
-                
-            zero = max(zero, prev + curr)
-            prev = curr
+            else:
+                if prev != 0 and curr != 0:
+                    best = max(best, prev + curr)
+                if curr != 0:
+                    prev = curr
+                curr = 0
 
-        return one + zero - 2
+        return best + count
